c754c43163c34430f519682c49d934ba53fcdc13
Mwambui123 <maurinawambuia@gmail.com>
2023-12-11 10:06:19 +0300

    remove vowels

--- lib/consonantvalues/consonant.py
+++ lib/consonantvalues/consonant.py
@@ -1,3 +1,6 @@
-def consonant ():
+def consonant (s):
     def sub_string(sub):
         return sum(ord(char) - ord('a') + 1 for char in sub)
+    
+    #remove vowels
+    consonant_str = ''.join(char for char in s if char not in 'aeiou')

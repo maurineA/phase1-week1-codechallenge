7d9cbdbae305a6f935ebb44a0792fba662a61c31
Mwambui123 <maurinawambuia@gmail.com>
2023-12-11 08:55:12 +0300

    check if the positive numbers are being printed to true

--- lib/numbers/numbers.py
+++ lib/numbers/numbers.py
@@ -1,5 +1,10 @@
-def two_positive_numbers(a,b,c):
-    positive_num = sum(1 for num in [a,b,c] if num > 0)
+def two_positive_numbers(a, b, c):
+    positive_num = sum(1 for num in [a, b, c] if num > 0)
     return positive_num == 2
 
 print(two_positive_numbers(2, 4, -3))
+print(two_positive_numbers(-4, 6, 8))
+print(two_positive_numbers(4, -6, 9))
+print(two_positive_numbers(-4, 6, 0))
+print(two_positive_numbers(4, 6, 10))
+print(two_positive_numbers(-14, -3, -4))
